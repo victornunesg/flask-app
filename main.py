9b72ee1867385ff8d28421eb58f7dcc9801db52a
Victor Nunes <victorngomes@gmail.com>
2023-04-18 12:07:19 -0300

    testando bd remoto novamente

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ from webblog import app, database
 # garante que o código abaixo irá rodar somente se o arquivo em questao estiver rodando
 if __name__ == '__main__':
     # debug=True para não ser necessário resetar o código após novas alterações, basta F5 no browser
-    with app.app_context():
-        database.create_all()
     app.run(debug=True)
