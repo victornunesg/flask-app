0c204cb17f4337aba82fb32e4d41a1aec828d926
Victor Nunes <114002524+victornunesg@users.noreply.github.com>
2023-07-11 17:45:56 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from webblog import app
-
-""" De acordo com a estrutura do código, o arquivo main é utilizado apenas para rodar o app. O código abaixo garante que
-o app irá rodar somente se o arquivo em questão, 'main.py', estiver rodando. """
-
-if __name__ == '__main__':
-    # debug=True para não ser necessário resetar o código após novas alterações, basta F5 no browser
-    app.run(debug=True)
